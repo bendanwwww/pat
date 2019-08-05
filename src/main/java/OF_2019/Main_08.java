854a6d4c1b17bf250ecfa25a65b3459f2e159855
liushuoyang <liushuoyang@100tal.com>
2019-08-05 21:46:40 +0800

    08_tmp 09_tmp

--- src/main/java/OF_2019/Main_08.java
+++ src/main/java/OF_2019/Main_08.java
@@ -1,6 +1,10 @@
 package OF_2019;
 
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * 题目描述
  * 牛牛准备参加学校组织的春游, 出发前牛牛准备往背包里装入一些零食, 牛牛的背包容量为w。
@@ -22,7 +26,44 @@ package OF_2019;
  */
 public class Main_08 {
 
+    static int res = 1;
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int foodNumber = sc.nextInt();
+        int bagVolume = sc.nextInt();
+
+        List<Integer> foodList = new ArrayList<Integer>();
+        long all = 0;
+        for(int i = 0 ; i < foodNumber ; i++) {
+            foodList.add(sc.nextInt());
+            all = all + (long) foodList.get(i);
+        }
+
+        if(all <= bagVolume) {
+            res = (int) Math.pow(2, foodNumber);
+            System.out.println(res);
+        }else{
+            count(foodList, 0, 0, bagVolume);
+            System.out.println(res);
+        }
 
     }
+
+    private static void count(List<Integer> foodList, long allFood, int state, int total) {
+        if (state >= foodList.size()) {
+            return;
+        }
+        if (allFood > total) {
+            return;
+        }
+        // 不添加
+        count(foodList, allFood, state + 1, total);
+        // 添加
+        if (allFood + foodList.get(state) <= total) {
+            res++;
+            count(foodList, allFood + foodList.get(state), state + 1, total);
+        }
+    }
+
 }
