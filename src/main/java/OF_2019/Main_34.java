5befdbdf1e06e4551b810517eec82e710a112e7d
liushuoyang <liushuoyang@100tal.com>
2019-08-30 18:00:10 +0800

    34

--- src/main/java/OF_2019/Main_34.java
+++ src/main/java/OF_2019/Main_34.java
@@ -1,6 +1,8 @@
 package OF_2019;
 
 
+import java.util.Scanner;
+
 /**
  * 题目描述
  * 输入年、月、日，计算该天是本年的第几天。
@@ -27,5 +29,28 @@ public class Main_34 {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        String[] ins = sc.nextLine().split(" ");
+
+        int[] days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        int out = 0;
+
+        int year = Integer.parseInt(ins[0]);
+        int month = Integer.parseInt(ins[1]);
+        int day = Integer.parseInt(ins[2]);
+
+        for(int i = 0 ; i < month - 1 ; i++) {
+            out = out + days[i];
+        }
+
+        out = out + day;
+
+        if(year % 4 == 0 && year % 100 != 0 && month > 2) {
+            out++;
+        }
+
+        System.out.println(out);
     }
 }
